e2c6256a06069211db9e8b5d28737c077f5b4365
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-13 19:05:36 +0100

    initial commit

---
+++ src/main/java/ma/usmba/appsecurityservvice/AppSecurityServviceApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.appsecurityservvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityServviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSecurityServviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/appsecurityservvice/AppSecurityServviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.appsecurityservvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityServviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
